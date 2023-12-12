77168b7b4b7d34534398ce2bf7213b869f746c3c
Hadhri <rihemhadhri@gmail.com>
2023-12-12 23:56:02 +0100

    commit

---
+++ mongoDB/Mongoose/jokes_api/routes/jokes.routes.js
@@ -0,0 +1,8 @@
+const JokesController = require('../controllers/jokes.controller');
+
+module.exports = app => {
+    app.get('/api/jokes', JokesController.findAllJokes);
+    app.get('/api/jokes/:id', JokesController.findOneSingleJoke);
+    app.patch('/api/jokes/:id', JokesController.updateExistingJoke);
+    app.post('/api/jokes', JokesController.createNewJoke);
+    app.delete('/api/jokes/:id', JokesController.deleteAnExistingJoke); }
